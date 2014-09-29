e7d8b8710b556908d6f95fae09e1884124b2a3b0
Sophie Cooper <sophie.cooper@7digital.com>
2014-09-29 15:39:14 +0100

    Added web project and wrote tests for checking web response

---
+++ CheckoutKataApi.Tests/WalkingSkeleton.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Net;
+using NUnit.Framework;
+
+namespace CheckoutKataApi.Tests
+{
+	[TestFixture]
+
+    public class WalkingSkeletonTests
+    {
+		[Test]
+		public void Should_return_message()
+		{
+			var request = WebRequest.Create("http://checkout-kata.local/");
+			var response = (HttpWebResponse) request.GetResponse();
+
+			var inputStream = response.GetResponseStream();
+			var streamReader = new StreamReader(inputStream);
+			var responseStream = streamReader.ReadToEnd();
+
+			Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+			Assert.That(responseStream, Is.EqualTo("Hello World!"));
+		}
+    }
+}
---
+++ CheckoutKataApi.Web/HttpHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CheckoutKataApi.Web
+{
+	public class HttpHandler : IHttpHandler
+	{
+		public void ProcessRequest(HttpContext context)
+		{
+			context.Response.Write("Hello World!");
+		}
+
+		public bool IsReusable { get; private set; }
+	}
+}
